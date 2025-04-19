1f92b7ee7efbc5782ed7f6f70e661a2bf090c181
majida-elghadiri <majida.elghadiri@etu.uae.ac.ma>
2025-04-20 00:41:18 +0200

    ajout du formulaire ajouter-question.html

---
+++ script.js
@@ -0,0 +1,28 @@
+// TODO : Ajouter un écouteur d'événement sur le formulaire}
+document.getElementById('form-examen').addEventListener('submit', function(e)
+{
+e.preventDefault();
+// TODO : Récupérer les valeurs du formulaire
+const examen = {
+  nom: document.getElementById('nom').value,
+  duree: parseInt(document.getElementById('duree').value),
+  description: document.getElementById('description').value,
+  proprietaire: document.getElementById('proprietaire').value,
+  questions: [],
+  id : generateUniqueId(), 
+};
+// TODO : Sauvegarder l'examen dans le localStorage sous une clé basée sur
+//le nom du propriétaire
+
+const examsKey = 'examens_' + examen.proprietaire;
+const exams = JSON.parse(localStorage.getItem(examsKey)) || [];
+exams.push(examen);
+localStorage.setItem(examsKey, JSON.stringify(exams));
+
+alert('Examen ajouté avec succès !');
+this.reset();
+});
+
+function generateUniqueId() {
+  return Date.now().toString(36) + Math.random().toString(36).substr(2, 5);
+}
